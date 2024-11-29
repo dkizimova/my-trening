26981bf643ceeec31b5b8995caa96d951d363129
dkizimova <diljara06@gmail.com>
2024-11-29 23:41:41 +0300

    Update module_2_4new.py

--- module_2_4.py
+++ module_2_4.py
@@ -1,15 +1,28 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+n = 0 # Проверямое число
+
 primes = []
 not_primes = []
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-for i in numbers:
-    for j in range(1, 15):
-        a = f'{i}//{j}={i // j}'
-        print(a)
-        if i > 1 and (i % 2 != 0 or i == 2) and (i % 3 != 0 or i == 3):
-            primes.append(i)
+i = 0
+for i in range(len(numbers)):
+    is_prime = True
+    n = numbers[i]  # Проверямое число
+
+    if n < 2:   # не 0 и не 1
+        print(n, '- не простое и не сложное число')
+        continue
+    else:
+
+        f = n ** (1 / 2)  # Корень квадратный из n
+    for a in range(2, int(f + 1)):
+        if n % a == 0:
+            is_prime = False
             break
-        else:
-            not_primes.append(i)
-            break
-print(not_primes)
-print(primes)
+
+    if not (is_prime):
+        not_primes.append(n)
+    else:
+        primes.append(n)
+is_prime = True  # признак простого числа
+print('Простые числа ', primes)
+print('Составные числа', not_primes)
