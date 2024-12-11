7505fa4b8eabb3dafa7f6ef93c828c779c8da2ce
dkizimova <diljara06@gmail.com>
2024-12-11 22:17:07 +0300

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -8,8 +8,5 @@ def single_root_words(root_word, *other_words):
     return same_words
 
 
-result1 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
-result2 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
-
-print(result1)
-print(result2)
+print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
+print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies'))
